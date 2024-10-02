b0b50e51db717cb7f1a161849a59584cd615e704
Gavin Tang <gav@gavintang.dev>
2024-10-02 08:49:53 -0500

    add harpoon

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -3,9 +3,15 @@
 --
 -- See the kickstart.nvim README for more information
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
 return {
   {
     'stevearc/oil.nvim',
     opts = {},
+  },
+  {
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
   },
 }
